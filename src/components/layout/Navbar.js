71d67a7c18241be61f0a2acf5bf8524ffaa9ce0c
Ahmed Ezzat <moreone25@yahoo.com>
2020-01-29 22:51:10 +0200

    initial commit

---
+++ src/components/layout/Navbar.js
@@ -0,0 +1,28 @@
+import React  from 'react'
+import PropTypes from 'prop-types'
+
+//destructures props
+ const Navbar = ({icon,title}) => {
+    
+        return (
+            <nav className='navbar bg-primary'>
+                <h1>
+                    <i className={icon}/> {title}
+                </h1>
+            </nav>
+        );
+
+};
+
+Navbar.defaultProps ={
+    title:'Github Finder',
+    icon:'fab fa-github'
+};
+
+Navbar.propTypes = {
+    title: PropTypes.string.isRequired,
+    icon: PropTypes.string.isRequired
+};
+
+
+export default Navbar
---
+++ src/components/users/UserItem.js
@@ -0,0 +1,24 @@
+import React  from 'react'
+import PropTypes from 'prop-types'
+
+//destructure
+ const UserItem = ({user:{login,avatar_url,html_url}}) => {
+   
+        // const {login,avatar_url,html_url} = props.user;
+        return (
+            <div className="card text-center">
+                <img src={avatar_url} alt="" className="round-img" style={{width:'60px'}}/>
+                <h3>{login}</h3>
+                <div>
+                    <a href={html_url} className="btn btn-dark btn-sm my-1">
+                        more
+                    </a>
+                </div>
+            </div>
+        )
+}
+
+UserItem.propTypes={
+    user:PropTypes.object.isRequired
+};
+export default UserItem
