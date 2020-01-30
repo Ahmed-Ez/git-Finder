447edce88763c3593a686a31939d0dafb7c25ba5
Ahmed Ezzat <moreone25@yahoo.com>
2020-01-30 23:07:20 +0200

    replace fixed state with api data using axios

--- src/App.js
+++ src/App.js
@@ -2,14 +2,28 @@ import React from 'react';
 import './App.css';
 import Navbar from './components/layout/Navbar';
 import Users from './components/users/Users';
+import axios from 'axios';
 
 class App extends React.Component {
+
+  state = {
+    users:[],
+    loading:false
+  };
+
+  async componentDidMount(){
+    this.setState({loading:true});
+    const res = await axios.get(`https://api.github.com/users?client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}
+    &client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`);
+    this.setState({users:res.data,loading:false});
+  }
+
   render(){
     return (
       <div className="App">
         <Navbar title="Github Finder" icon="fab fa-github"/>
         <div className="container">
-          <Users />
+          <Users loading={this.state.loading} users={this.state.users} />
         </div>
         
       </div>
