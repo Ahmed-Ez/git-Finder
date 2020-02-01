686067f5eeadefb9af199dbe6aa8036cf5dd8804
Ahmed Ezzat <moreone25@yahoo.com>
2020-02-01 20:46:35 +0200

    Add Router and about page

--- src/App.js
+++ src/App.js
@@ -1,54 +1,71 @@
-import React from 'react';
+import React, { Fragment } from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import './App.css';
 import Navbar from './components/layout/Navbar';
 import Users from './components/users/Users';
 import axios from 'axios';
-import Search from './components/users/Search'
-import Alert from './components/layout/Alert'
+import Search from './components/users/Search';
+import Alert from './components/layout/Alert';
+import About from './components/pages/About'
 
 class App extends React.Component {
-
   state = {
-    users:[],
-    loading:false,
-    alert:null
+    users: [],
+    loading: false,
+    alert: null
   };
 
-  async componentDidMount(){
-    this.setState({loading:true});
+  async componentDidMount() {
+    this.setState({ loading: true });
     const res = await axios.get(`https://api.github.com/users?client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}
     &client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`);
-    this.setState({users:res.data,loading:false});
+    this.setState({ users: res.data, loading: false });
   }
 
-  searchUsers =  async text => {
-    this.setState({loading:true});
+  searchUsers = async text => {
+    this.setState({ loading: true });
     const res = await axios.get(`https://api.github.com/search/users?q=${text}&client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}
     &client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`);
-    this.setState({users:res.data.items,loading:false});
-  }
-
-  clearUsers = () => this.setState({users:[],loading:false});
-  
-  setAlert = (msg,type) =>{
-    this.setState({alert:{msg,type}})
-
-    setTimeout(()=>this.setState({alert:null}),5000);
+    this.setState({ users: res.data.items, loading: false });
   };
 
-  render(){
-    const {loading,users,alert} = this.state;
+  clearUsers = () => this.setState({ users: [], loading: false });
+
+  setAlert = (msg, type) => {
+    this.setState({ alert: { msg, type } });
+
+    setTimeout(() => this.setState({ alert: null }), 5000);
+  };
+
+  render() {
+    const { loading, users, alert } = this.state;
     return (
-      <div className="App">
-        <Navbar title="Github Finder" icon="fab fa-github"/>
-        <Alert alert={alert} />
-        <div className="container">
-          <Search searchUsers={this.searchUsers} clearUsers={this.clearUsers} 
-          users={users} setAlert={this.setAlert}/>
-          <Users loading={loading} users={users} />
+      <Router>
+        <div className="App">
+          <Navbar title="Github Finder" icon="fab fa-github" />
+          <div className="container">
+            <Alert alert={alert} />
+            <Switch>
+              <Route
+                exact
+                path="/"
+                render={props => (
+                  <Fragment>
+                    <Search
+                      searchUsers={this.searchUsers}
+                      clearUsers={this.clearUsers}
+                      users={users}
+                      setAlert={this.setAlert}
+                    />
+                    <Users loading={loading} users={users} />
+                  </Fragment>
+                )}
+              />
+              <Route exact path='/about' component={About}/>
+            </Switch>
+          </div>
         </div>
-        
-      </div>
+      </Router>
     );
   }
 }
---
+++ src/components/pages/About.js
@@ -0,0 +1,12 @@
+import React, { Fragment } from 'react';
+
+const About = () => {
+  return (
+    <Fragment>
+      <h1>About this app</h1>
+      <p>Search github users</p>
+    </Fragment>
+  );
+};
+
+export default About;
